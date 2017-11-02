c831296bf610dc2592a93e9debf6c3fe7a39a58a
moscowjane <janedesbarats91@gmail.com>
2017-11-02 22:19:24 +0000

    map

--- arrayMap.js
+++ arrayMap.js
@@ -3,7 +3,12 @@ var input = [
   { x: 12, y: 5 },
   { x: 8, y: 15 }
 ];
-var result = input.map(/* Your code here */);
+var result = input.map(function(currentValue, index, array)
+
+{
+  return Math.sqrt((Math.pow(currentValue.x,2)) + (Math.pow(currentValue.y,2)));
+
+});
 
 console.log(result[0] === 5);
 console.log(result[1] === 13);
